c42f7298545e6fcd4541ec97af3064080c7e150b
Andrey Kokora <andrey18121996@gmail.com>
2023-03-25 11:26:56 +0300

    create first slider

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 const swiper = new Swiper('.auctions__slider', {
+    navigation: {
+        nextEl: '.swiper-button-next1',
+        prevEl: '.swiper-button-prev1', // Включить стрелочки
+    },
     slidesPerView: 2.8,
     autoHeight: true,
     grabCursor: true,
